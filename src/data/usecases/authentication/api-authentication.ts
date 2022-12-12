5bc39b4e510b86a628074184be0aa854a0d74871
Matheus Rian <souzamatheusrian@gmail.com>
2022-12-12 11:37:06 -0300

    feat: ensure ApiAuthentication throw UnexpectedError if HttpPostClient returns 404

--- src/data/usecases/authentication/api-authentication.ts
+++ src/data/usecases/authentication/api-authentication.ts
@@ -13,8 +13,9 @@ export class ApiAuthentication implements Authentication {
 		const httpResponse = await this.httpPostClient.post({ url: this.url, body: params });
 
 		switch (httpResponse.statusCode) {
+		case HttpStatusCode.ok: break;
 		case HttpStatusCode.unauthorized: throw new InvalidCredentialsError();
-		case HttpStatusCode.badRequest: throw new UnexpectedError();
+		default: throw new UnexpectedError();
 		}
 	}
 }
