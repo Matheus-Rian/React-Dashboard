25921c90176ac4ac15dfaa4a1027fd83a2155abb
Matheus Rian <souzamatheusrian@gmail.com>
2022-12-11 22:12:25 -0300

    chore: config jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	roots: ['<rootDir>/src'],
+	collectCoverageFrom: [
+		'<rootDir>/src/**/*.{ts,tsx}',
+		// '!<rootDir>/src/main/**/*',
+		// '!<rootDir>/src/**/index.ts',
+		// '!**/*.d.ts'
+	],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'node',
+	transform: {
+		'.+\\.(ts|tsx)$': 'ts-jest'
+	},
+	moduleNameMapper: {
+		'@/tests/(.*)': '<rootDir>/tests/$1',
+		'@/(.*)': '<rootDir>/src/$1',
+		'\\.scss$': 'identity-obj-proxy'
+	}
+};
