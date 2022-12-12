841fa28cc682a3eddc833ca64b05dfefe27b0e3e
Matheus Rian <souzamatheusrian@gmail.com>
2022-12-11 23:48:44 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/authentication/api-authentication.spec.ts
+++ src/data/usecases/authentication/api-authentication.spec.ts
@@ -1,12 +1,26 @@
-import { HttpClientSpy } from '@/data/tests';
+import { HttpPostClientSpy } from '@/data/tests';
 import { ApiAuthentication } from './api-authentication';
 
+type SutTypes = {
+	sut: ApiAuthentication,
+	httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string): SutTypes => {
+	const httpPostClientSpy = new HttpPostClientSpy();
+	const sut = new ApiAuthentication(url, httpPostClientSpy);
+
+	return {
+		sut,
+		httpPostClientSpy
+	};
+};
+
 describe('ApiAuthentication', () => {
-	it('Should call HttpClient with correct URL', () => {
+	it('Should call HttpClient with correct URL', async () => {
 		const url = 'any_url';
-		const httpPostClientSpy = new HttpClientSpy();
-		const sut = new ApiAuthentication(url, httpPostClientSpy);
-		sut.auth();
+		const { sut, httpPostClientSpy } = makeSut(url);
+		await sut.auth();
 		expect(httpPostClientSpy.url).toBe(url);
 	});
 });
