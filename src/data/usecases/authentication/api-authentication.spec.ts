6ebc204e7d7690b05434c8ffedd072f70fa8ff7c
Matheus Rian <souzamatheusrian@gmail.com>
2022-12-12 00:26:22 -0300

    test: ensure ApiAuthentication calls HttpClient correct body

--- src/data/usecases/authentication/api-authentication.spec.ts
+++ src/data/usecases/authentication/api-authentication.spec.ts
@@ -1,13 +1,14 @@
 import { HttpPostClientSpy } from '@/data/tests';
 import { ApiAuthentication } from './api-authentication';
 import faker from 'faker';
+import { mockAuthentication } from '@/domain/tests/mock-authentication';
 
 type SutTypes = {
 	sut: ApiAuthentication,
 	httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url: string): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
 	const httpPostClientSpy = new HttpPostClientSpy();
 	const sut = new ApiAuthentication(url, httpPostClientSpy);
 
@@ -21,7 +22,14 @@ describe('ApiAuthentication', () => {
 	it('Should call HttpClient with correct URL', async () => {
 		const url = faker.internet.url();
 		const { sut, httpPostClientSpy } = makeSut(url);
-		await sut.auth();
+		await sut.auth(mockAuthentication());
 		expect(httpPostClientSpy.url).toBe(url);
+	});
+
+	it('Should call HttpClient with correct body', async () => {
+		const { sut, httpPostClientSpy } = makeSut();
+		const authParams = mockAuthentication();
+		await sut.auth(authParams);
+		expect(httpPostClientSpy.body).toBe(authParams);
 	});
 });
