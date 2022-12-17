ddb6c56f871a92dc6a0156946ffe99f694811723
Matheus Rian <souzamatheusrian@gmail.com>
2022-12-17 18:55:46 -0300

    test: ensure Login call authentication with correct values

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,21 +1,36 @@
 import { Login } from '.';
 import { Helper, ValidationStub } from '@/presentation/tests';
-import { cleanup, render, RenderResult } from '@testing-library/react';
+import { cleanup, render, RenderResult, fireEvent } from '@testing-library/react';
 import React from 'react';
 import faker from 'faker';
+import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication';
+import { AccountModel } from '@/domain/models';
+import { mockAccount } from '@/domain/tests/mock-authentication';
+
+class AuthenticationSpy implements Authentication {
+	params: AuthenticationParams;
+	account = mockAccount();
+
+	async auth(params: AuthenticationParams): Promise<AccountModel> {
+		this.params = params;
+		return Promise.resolve(this.account);
+	}
+
+}
 
 type SutTypes = {
 	sut: RenderResult
-	validationStub: ValidationStub
+	authenticationSpy: AuthenticationSpy
 }
 
 const makeSut = (validationError = ''): SutTypes => {
 	const validationStub = new ValidationStub();
+	const authenticationSpy = new AuthenticationSpy();
 	validationStub.error = validationError;
-	const sut = render(<Login validation={validationStub} />);
+	const sut = render(<Login validation={validationStub} authentication={authenticationSpy} />);
 	return {
 		sut,
-		validationStub
+		authenticationSpy
 	};
 };
 
@@ -46,5 +61,30 @@ describe('Login Page', () => {
 		Helper.populateField(sut, 'email');
 		Helper.populateField(sut, 'password');
 		Helper.testButtonIsDisabled(sut, false);
+	});
+
+	// it('Should show progress on submit', () => {
+	// 	const { sut } = makeSut();
+	// 	Helper.populateField(sut, 'email');
+	// 	Helper.populateField(sut, 'password');
+	// 	const button = sut.getByRole('button');
+	// 	fireEvent.click(button);
+	// 	// expect(button).toBeFalsy();
+	// 	Helper.testElementExists(sut, 'progress').toBeTruthy();
+	// });
+
+	it('Should call authentication with correct values', () => {
+		const { sut, authenticationSpy } = makeSut();
+		const email = faker.internet.email();
+		const password = faker.internet.password();
+
+		Helper.populateField(sut, 'email', email);
+		Helper.populateField(sut, 'password', password);
+		const button = sut.getByRole('button');
+		fireEvent.click(button);
+		expect(authenticationSpy.params).toEqual({
+			email,
+			password
+		});
 	});
 });
