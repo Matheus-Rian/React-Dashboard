4b93854431a68a971a8ea29edab06ff3949ceffe
Matheus Rian <souzamatheusrian@gmail.com>
2022-12-19 12:01:29 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -2,16 +2,18 @@ import { InvalidFieldError } from '@/validation/errors';
 import { MinLengthValidation } from './min-length-validation';
 import faker from 'faker';
 
+const makeSut = () => new MinLengthValidation(faker.database.column(), 5);
+
 describe('MinLengthValidation', () => {
 	it('Should return error if value is invalid', () => {
-		const sut = new MinLengthValidation(faker.database.column(), 5);
-		const error = sut.validate('123');
+		const sut = makeSut();
+		const error = sut.validate(faker.random.alphaNumeric(4));
 		expect(error).toEqual(new InvalidFieldError());
 	});
 
 	it('Should return falsy if value is valid', () => {
-		const sut = new MinLengthValidation(faker.database.column(), 5);
-		const error = sut.validate('12345');
+		const sut = makeSut();
+		const error = sut.validate(faker.random.alphaNumeric(5));
 		expect(error).toBeFalsy();
 	});
 });
