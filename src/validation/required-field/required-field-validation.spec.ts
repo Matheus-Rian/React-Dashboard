be0ee222c9fb0a0acf5ce9b784c3e3270fb43ddb
Matheus Rian <souzamatheusrian@gmail.com>
2022-12-18 21:30:20 -0300

    test: ensure RequiredFieldValidation return falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -8,4 +8,10 @@ describe('RequiredFieldValidation', () => {
 		const error = sut.validate('');
 		expect(error).toEqual(new RequiredFieldError());
 	});
+
+	it('Should return falsy if field is not empty', () => {
+		const sut = new RequiredFieldValidation('email');
+		const error = sut.validate(faker.internet.email());
+		expect(error).toBeFalsy();
+	});
 });
