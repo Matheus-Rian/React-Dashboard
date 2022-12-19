5f96d5ea799bd4a6702bca9d63ed21eed7eb71b7
Matheus Rian <souzamatheusrian@gmail.com>
2022-12-18 22:31:23 -0300

    feat: ensure EmailValidation return falsy if email is valid

--- src/validation/validators/email-validation/email-validation.ts
+++ src/validation/validators/email-validation/email-validation.ts
@@ -5,6 +5,7 @@ export class EmailValidation implements FieldValidation {
 	constructor(readonly name: string) {}
 
 	validate(value: string): Error {
-		return new InvalidFieldError();
+		const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+		return (!value || emailRegex.test(value)) ? null : new InvalidFieldError();
 	}
 }
