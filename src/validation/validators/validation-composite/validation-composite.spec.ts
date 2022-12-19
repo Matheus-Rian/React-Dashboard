1333a95ab9d7a6d1aef134f98140b0dfdcceea99
Matheus Rian <souzamatheusrian@gmail.com>
2022-12-19 12:38:53 -0300

    test: ensure ValidationComposite return the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,14 +4,15 @@ import { ValidationComposite } from './validation-composite';
 describe('ValidationComposite', () => {
 	it('Should return error if any validation fails', () => {
 		const fieldValidationSpy = new FieldValidationSpy('field');
+		fieldValidationSpy.error = new Error('first_error_message');
 		const fieldValidationSpy2 = new FieldValidationSpy('field');
-		fieldValidationSpy2.error = new Error('any_message');
+		fieldValidationSpy2.error = new Error('second_error_message');
 		const sut = new ValidationComposite([
 			fieldValidationSpy,
 			fieldValidationSpy2
 		]);
 
 		const error = sut.validate('field', 'any_value');
-		expect(error).toBe('any_message');
+		expect(error).toBe('first_error_message');
 	});
 });
